29b08ed9cf83bc088f97f8ea5898d26ac4416690
katherineRwhite <126996027+katherineRwhite@users.noreply.github.com>
2026-05-08 19:41:16 +0000

    Deploying to gh-pages from @ katherineRwhite/katherineRwhite.github.io@d7dc4b39a64d1d5fd1ba2df28d6210026d7bf5b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -401,12 +401,7 @@ ninja.data = [{
             },},{id: "news-new-paper-published-in-transportation-research-part-d-transport-and-environment-methodological-advancements-to-estimate-the-climate-impacts-of-household-travel-in-vancouver-with-alexander-bigazzi-elmira-berjisian-and-lawrence-frank-read-the-paper-here",
           title: 'New paper published in Transportation Research Part D: Transport and Environment! “Methodological advancements...',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
