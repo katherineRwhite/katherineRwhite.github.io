71bf079c94e75ac8901c7fae586fbe25efb14721
katherineRwhite <126996027+katherineRwhite@users.noreply.github.com>
2026-05-09 15:47:33 +0000

    Deploying to gh-pages from @ katherineRwhite/katherineRwhite.github.io@9ed4e108e95b98d55110a636ecb3d0dd4ea7ee47 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My academic CV is available to download above. This page provides an overview of my education, research experience, publications, and service.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
